f082cbe9cc57076c87ebc3cc73ddeaf070357298
Rotlesnake <rotlesnake@yandex.ru>
2023-08-04 12:04:14 +0300

    Update FileController.php

--- src/App/Uploads/Controllers/FileController.php
+++ src/App/Uploads/Controllers/FileController.php
@@ -19,12 +19,13 @@ class FileController extends \MapDapRest\Controller
     public function anyAction($request, $response, $type, $table, $filename)
     {
       if (ob_get_level()) {ob_end_clean();}
+      $orig_filename = substr($filename[0], strpos($filename[0],"_file_")+6 );
       $file = ROOT_PATH."uploads/".$table."/".$filename[0];
       if (!file_exists($file)) { return ""; }
 
       header('Content-Description: File Transfer');
       header('Content-Type: application/octet-stream');
-      header('Content-Disposition: attachment; filename=' . basename($file));
+      header('Content-Disposition: attachment; filename=' . $orig_filename);
       header('Content-Transfer-Encoding: binary');
       header('Expires: 0');
       header('Cache-Control: must-revalidate');
