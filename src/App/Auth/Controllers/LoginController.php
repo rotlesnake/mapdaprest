a34f7dd47492835c231964deae395ed22122b1f7
Rotlesnake <rotlesnake@yandex.ru>
2023-10-09 16:45:29 +0300

    Update LoginController.php

--- src/App/Auth/Controllers/LoginController.php
+++ src/App/Auth/Controllers/LoginController.php
@@ -11,32 +11,31 @@ class LoginController extends \MapDapRest\Controller
     public function __construct($app, $request, $response, $args)
     {
         $this->APP = $app;
-        if ($request->hasParam("login")) {
+        if ($request->hasParam("login") && $request->hasParam("password")) {
             $data = ["login"=>$request->getParam("login"), "password"=>$request->getParam("password")];
-            $this->APP->auth->login($data);
-            if (!$this->APP->auth->isGuest()) return;
+            return $this->APP->auth->login($data);
         }
         if ($request->hasParam("token")) {
             $data = ["token"=>$request->getParam("token")];
-            $this->APP->auth->login($data);
-            if (!$this->APP->auth->isGuest()) return;
-        }        
-        return $response->sendError(["message"=>"Ошибка в логине или пароле"], 500);
+            return $this->APP->auth->login($data);
+        }
     }
 
 
 
     public function indexAction($request, $response, $params) {
        if ($this->APP->auth->isGuest()) {
-           if ($request->hasHeader('token')) { 
-               $response->sendError(["message"=>"Токен просрочен либо не действителен"], 401);
-           }
-           $response->sendError(["message"=>"Пользователь не найден"], 401);
+           $response->setResponseCode(401);
+           $message = "Ошибка в логине или пароле";
+           if ($request->hasParam("token")) $message = "Ошибка в токене";
+           if ($request->hasParam("password")) $message = "Ошибка в логине или пароле";
+           return ["error"=>1, "message"=>$message];
+           die();
        }
 
        $user = $this->APP->auth->getFields();
        \MapDapRest\App\Auth\Events\Emits::userLogin($this->APP->auth);
-       $response->sendSuccess(["token"=>$user["token"], "user"=>$user]);
+       return ["status"=>1, "token"=>$user["token"], "user"=>$user];
     }
 
 }
